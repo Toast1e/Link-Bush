73a54d1156ae9ce4223498d627d86137912d5248
Toast1e <64542429+Toast1e@users.noreply.github.com>
2023-05-21 09:58:16 +0200

    V4

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function init() {
 
 const expandButton = document.getElementById('expand-button');
 const expandDiv = document.getElementById('expand-div');
-const closeButton = document.getElementById('close-button');
+const closeButton = document.getElementById('submit');
 
 expandButton.addEventListener('click', () => {
   if (expandDiv.style.display === 'block') {
